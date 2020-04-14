e5f8c54140db383ff464547baad786bd10aa07d1
Brod8362 <brodriguez410@gmail.com>
2020-04-14 17:12:27 -0500

    remove packet segment type in favor of u8, finally successful connection

--- src/main.rs
+++ src/main.rs
@@ -4,30 +4,24 @@ use std::time;
 use notify_rust::Notification;
 use std::thread::sleep;
 use std::str;
-
-enum PacketSegmentType {
-    Title = 0x01,
-    Body = 0x02,
-    Image = 0x03,
-    End = 0x7F
-}
+use std::collections::HashMap;
 
 fn start_notification() {
-    Notification::new()
+    let result = Notification::new()
         .summary("LinuxSync")
         .body("Searching for phone...")
         .show();
 }
 
 fn connection_established(addr: SocketAddr) {
-    Notification::new()
+    let result = Notification::new()
         .summary("LinuxSync")
         .body(format!("Connected to {}", addr).as_str())
         .show();
 }
 
 fn connection_lost(addr: SocketAddr) {
-    Notification::new()
+    let result = Notification::new()
         .summary("LinuxSync")
         .body(format!("Connection to {} lost.", addr).as_str())
         .show();
@@ -42,32 +36,37 @@ fn notification(title: &str, text: &str) {
 
 fn data_handler(mut data: &[u8], size: usize) {
     assert_eq!(data[0], 0x3C);
-    let segments = data[1].to_usize();
+    println!("{:?}", data);
+    let mut elements: HashMap<u8, &str> = HashMap::new();
+    let segments = data[1] as usize;
     let mut read = 2;
     for x in 0..segments {
-        let segment_type = data[read].to_usize();
+        let segment_type = data[read];
         println!("Type is {}", segment_type);
         read+=1;
-        let segment_length = data[read].to_usize();
+        let segment_length = data[read] as usize;
         println!("Length is {}", segment_length);
         read+=1;
-        let data_r = std::str::from_utf8(&data[read..segment_length]);
+        let data_r = std::str::from_utf8(&data[read..read+segment_length]);
         read+=segment_length;
         if data_r.is_err() {
             //do something bad
             continue;
         }
         let data_f = data_r.unwrap();
-        println!("data: {}", data_f);
+        elements.insert(segment_type, data_f);
     }
-    assert_eq!(data[read+1], PacketSegmentType::End);
+    assert_eq!(data[read], 127); //if this assertion fails, invalid packet data was sent
+    notification(elements.get(&1).unwrap(),
+                 elements.get(&2).unwrap());
 }
 
 fn client_handler(mut stream: TcpStream) {
     let mut data = [0 as u8; 1024];
     while match stream.read(&mut data) {
         Ok(size) => {
-            if size != 0 {
+            println!("data read");
+            if size != 0 && data[0] == 0x3C {
                 data_handler(data.as_ref(), size);
             }
             true
